ea7007566fba1e32f8883d76d4a9bc2191c205c0
holyjack <holyjackdev@gmail.com>
2023-12-17 15:27:04 +0400

    wow

--- src/utils/Shapes/Shape.tsx
+++ src/utils/Shapes/Shape.tsx
@@ -7,6 +7,8 @@ import line, { name as l } from "./Line";
 import rectangle, { name as r } from "./Rect";
 import eraser, { name as e } from "./Eraser";
 import circle, { name as c } from "./Circle";
+import React from "react";
+import isEqual from "lodash.isequal";
 
 const tool = [];
 const ShapeMap: Record<string, ToolConfig<MyShapeConfigs>> = {};
@@ -28,12 +30,25 @@ const TypedShapeMap: Record<
   ToolConfig<MyShapeConfigs>
 > = ShapeMap as Record<Tool, ToolConfig<MyShapeConfigs>>;
 
-function MyShape(props: MyShapeConfigsWithTool) {
+function areEqual(
+  prevProps: MyShapeConfigsWithTool,
+  nextProps: MyShapeConfigsWithTool,
+) {
+  if (prevProps.tool !== nextProps.tool) return false;
+
+  const prevKeys = Object.keys(prevProps);
+  const nextKeys = Object.keys(nextProps);
+
+  if (prevKeys.length !== nextKeys.length) return false;
+  return isEqual(prevProps, nextProps);
+}
+
+const MyShape = React.memo((props: MyShapeConfigsWithTool) => {
   const { tool, ...componentProps } = props;
   if (!tool) return null;
   const { component: Component } = TypedShapeMap[tool as Tool];
   return <Component {...componentProps} />;
-}
+}, areEqual);
 
 export { MyShape };
 export default TypedShapeMap;
